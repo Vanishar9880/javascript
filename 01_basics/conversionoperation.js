52a51cf95cf67a0e44019eb814c95308e9f0a1d0
Vanishar9880 <144358345+Vanishar9880@users.noreply.github.com>
2026-03-09 19:56:39 +0000

    conversion dataypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "23"
+
+console.log(typeof score);
+console.log(typeof (score)); 
+
+let valueInumber = Number(score)
+
+console.log(typeof valueInumber);
+
+//33 => 33
+// 33abs => NaN not a number 
+// true => 1
+// undefined => NaN
+
+
+let issloggedin =1 
+let booleanisloggedin = Boolean(1);
+
+console.log(booleanisloggedin)
+
+// 1 => true , 0=> false 
+// "hitesh" =>true
+// "" => 0
+
