433a8fff505b8b9ca5911e5cd9912ea1182b54b0
Vanishar9880 <144358345+Vanishar9880@users.noreply.github.com>
2026-03-09 19:37:16 +0000

    config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+// alert(3+3) we are using node js not browser it will give error but will run in broswer 
+
+let name = "hitesh" 
+// string data type
+
+let age = 16
+// int datatype
+
+let isloggedin = true
+// boolean
+
+//number ==>> 2 to power 53 range 
+// bigint 
+//string
+//boolean
+// null standalone value 
+//undefined ex:- let state;
+//symbol  for unique
+
+//object 
+console.log(typeof "hitesh")
+
+console.log(tyoeof undefined); //undefined
+console.log(tyoeof null);// object
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountID = 12345 // const cannot be modified
+let accountemail ="vanisha@gmail.com"
+var accountpassword = "3434"
+accountcity= "agra"
+
+accountemail= "hc@gmail.com"
+accountcity = "delhi"
+
+console.table([accountemail, accountID ,accountpassword ,accountcity])
+
+// let and var can be chnaged
+/*
+prefer not to use var because of issue in block space and 
+ functional scope 
+*/
+
